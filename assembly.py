a22e6df5f4df2197b93bfaa7bd8b0254af4bba9d
Vitoria Lara <vitorialara.soria@gmail.com>
2024-09-06 14:06:08 -0700

    added mic transcription

--- assembly.py
+++ assembly.py
@@ -2,10 +2,10 @@ import assemblyai as aai
 
 aai.settings.api_key = "b22bc6fc4ce046f08fce971260e70ce6"
 
-FILE_URL = "https://github.com/AssemblyAI-Community/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
+#FILE_URL = "https://github.com/AssemblyAI-Community/audio-examples/raw/main/20230607_me_canadian_wildfires.mp3"
 
 # You can also transcribe a local file by passing in a file path
-# FILE_URL = './path/to/file.mp3'
+FILE_URL = '/Users/vitorialarasoria/Desktop/hackathon/output_audio.wav'
 
 transcriber = aai.Transcriber()
 transcript = transcriber.transcribe(FILE_URL)
